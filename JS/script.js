595fa24213b6a9c7b1257268279f45b3343bece7
Dfrmn <danikfurman2010@gmail.com>
2021-07-02 20:12:33 +0300

    1

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const arr = [1, 2, 3, -1, -2, -3];
+
+function func(arr){
+    const positiveNumberArr = [];
+
+    if(!arr.length){return `Массив пуст`;}
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i] > 0){
+                positiveNumberArr.push(arr[i]);
+            }
+        }
+        for (let z = 0; z < positiveNumberArr.length; z++) {
+            if (!positiveNumberArr.length) {
+                return null;
+            }
+            return positiveNumberArr;
+        }
+}
+alert(func());
